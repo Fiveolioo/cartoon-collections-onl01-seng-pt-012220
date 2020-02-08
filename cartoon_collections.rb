4c5570f0887aded35d0779a3a9ff53daf81c7c79
Richard Cho <96richardohc@gmail.com>
2020-02-08 07:58:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,18 +5,13 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(elements)
-  planeteer_calls = elements.collect do |element|
+  elements.collect do |element|
     element.capitalize().split('').push('!').join('')
   end
-  planeteer_calls
 end
 
 def long_planeteer_calls(calls)
-  if calls.any?{|i| i.length > 4}
-    true
-  else
-    false
-  end
+  calls.any?{|i| i.length > 4}
 end
 
 def find_the_cheese(ingredients)
